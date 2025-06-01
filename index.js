98afd4db6b11f0019a459af448785fcb7d055702
adiichoudhary <127790319+adiichoudhary@users.noreply.github.com>
2025-06-01 12:31:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
+        secure: false,  //process.env.NODE_ENV === 'production',
         httpOnly: true,
         maxAge: 1000 * 60 * 60 * 24 // 24 hours
     }
